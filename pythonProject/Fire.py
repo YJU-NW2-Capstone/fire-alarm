412871b631883c50f07d13fbe25da258a5f500ad
dlehdgur0304 <74841719+dlehdgur0304@users.noreply.github.com>
2025-02-12 17:26:41 +0900

    문서 나눔
    수정이 쉽도록 일단 문서를 나누기만 해놓은것

---
+++ pythonProject/Fire.py
@@ -0,0 +1,30 @@
+import sys
+import self
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import QTime
+from PyQt5.QtGui import  QPixmap
+
+from_class = uic.loadUiType("./Fire.ui")[0]
+
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        msgBox = QMessageBox()
+        msgBox.setText("정말로 창을 닫으시겠습니까?")
+        msgBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        msgBox.setDefaultButton(QMessageBox.No)
+
+        ret = msgBox.exec_()
+        if ret == QMessageBox.No:
+            event.ignore()
+        else:
+            event.accept()
+
+app = QApplication(sys.argv)
+mainWindow = WindowClass()
+mainWindow.show()
+app.exec_()
---
+++ pythonProject/ui/main_window.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+
+from_class = uic.loadUiType("./Fire.ui")[0]
+
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+app = QApplication(sys.argv)
+mainWindow = WindowClass()
+mainWindow.show()
+app.exec_()
