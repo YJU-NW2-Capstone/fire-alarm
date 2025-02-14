aad0729ded62ca137898213ab2b479a0dc6b0854
dlehdgur0304 <74841719+dlehdgur0304@users.noreply.github.com>
2025-02-14 17:29:47 +0900

    현재 페이지 표시 기능 추가

--- pythonProject/Fire.py
+++ pythonProject/Fire.py
@@ -14,13 +14,17 @@ class WindowClass(QMainWindow, from_class):
         self.setupUi(self)
         self.initUI()
 
-        # 페이지 전환 버튼 연결
-        self.pushButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
-        self.pushButton_2.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
-        self.pushButton_3.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        # 버튼 리스트 생성
+        self.buttons = [self.pushButton, self.pushButton_2, self.pushButton_3]
 
-        # 페이지 전환 시마다 업데이트
-        self.stackedWidget.currentChanged.connect(self.updateDateTime)
+        # 초기 페이지 설정 및 버튼 스타일 업데이트
+        self.stackedWidget.setCurrentIndex(0)
+        self.updateButtonStyles(self.pushButton)
+
+        # 페이지 전환 버튼 연결
+        self.pushButton.clicked.connect(lambda: self.changePage(0, self.pushButton))
+        self.pushButton_2.clicked.connect(lambda: self.changePage(1, self.pushButton_2))
+        self.pushButton_3.clicked.connect(lambda: self.changePage(2, self.pushButton_3))
 
     def initUI(self):
         # DateTimeLabel 생성
@@ -29,6 +33,9 @@ class WindowClass(QMainWindow, from_class):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateDateTime)
         self.timer.start(1000)
+
+        # 초기 날짜 및 시간 표시
+        self.updateDateTime()
 
     def updateDateTime(self):
         # 현재 날짜와 시간 가져오기
@@ -40,11 +47,9 @@ class WindowClass(QMainWindow, from_class):
         self.dateTimeLabel.adjustSize()
 
         # 레이블 위치 재조정
-        # 윈도우 크기 가져오기
         window_width = self.width()
         window_height = self.height()
 
-        # 퍼센트 위치 계산 (오른쪽 90%, 위쪽 5%)
         x_position = int(window_width * 0.9) - self.dateTimeLabel.width()
         y_position = int(window_height * 0.01)
 
@@ -52,6 +57,22 @@ class WindowClass(QMainWindow, from_class):
 
         # 다시 그리기
         self.dateTimeLabel.show()
+
+    def changePage(self, index, button):
+        self.stackedWidget.setCurrentIndex(index)
+        self.updateDateTime()
+        self.updateButtonStyles(button)
+
+    def updateButtonStyles(self, selectedButton):
+        default_style = 'background-color: blue; color: white;'
+        selected_style = 'background-color: red; color: white;'
+
+        # 모든 버튼에 기본 스타일 적용
+        for button in self.buttons:
+            button.setStyleSheet(default_style)
+
+        # 선택된 버튼에 선택된 스타일 적용
+        selectedButton.setStyleSheet(selected_style)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
